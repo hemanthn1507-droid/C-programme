b6741da81669a0cc1d5e1ce90d16141dde49383a
hemanthn1507-droid <hemanthn1507@gmail.com>
2026-07-08 14:38:46 +0530

    Create Odd serious.c

---
+++ Number Series/Odd serious.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+
+    printf("Enter the limit: ");
+    scanf("%d", &n);
+
+    printf("Odd numbers up to %d are:\n", n);
+
+    for(i = 1; i <= n; i += 2) {
+        printf("%d ", i);
+    }
+
+    return 0;
+}
