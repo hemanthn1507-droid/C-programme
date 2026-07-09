0641ca9e8ee770a4e1115a515a4541cc1257ff63
hemanthn1507-droid <hemanthn1507@gmail.com>
2026-07-09 11:08:28 +0530

    Create Squares Series.c

---
+++ Number Series/Squares Series.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int i, n;
+    printf("Enter limit: ");
+    scanf("%d", &n);
+
+    for(i = 1; i <= n; i++) {
+        printf("%d ", i * i);
+    }
+
+    return 0;
+}
