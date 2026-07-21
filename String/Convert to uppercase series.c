7462c240a49d8414583eda8b9fe772c7e5b53819
hemanthn1507-droid <hemanthn1507@gmail.com>
2026-07-21 09:37:14 +0530

    Create Convert to uppercase series.c

---
+++ String/Convert to uppercase series.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    char str[100];
+    int i=0, count=0;
+    scanf("%s", str);
+    while(str[i]) {
+        if((str[i]>='a' && str[i]<='z') &&
+           !(str[i]=='a'||str[i]=='e'||str[i]=='i'||str[i]=='o'||str[i]=='u'))
+            count++;
+        i++;
+    }
+    printf("Consonants = %d", count);
+}
