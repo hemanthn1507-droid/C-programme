9bb89788344147c2d1abd8c6cb7f281f244d061f
hemanthn1507-droid <hemanthn1507@gmail.com>
2026-07-21 09:23:34 +0530

    Create Concatenate two strings series.c

---
+++ String/Concatenate two strings series.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    char s1[100], s2[100];
+    int i=0, j=0;
+    scanf("%s %s", s1, s2);
+    while(s1[i] != '\0') i++;
+    while(s2[j] != '\0') {
+        s1[i] = s2[j];
+        i++; j++;
+    }
+    s1[i] = '\0';
+    printf("%s", s1);
+}
